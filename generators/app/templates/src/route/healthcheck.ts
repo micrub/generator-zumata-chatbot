c9ec323ee690f74a68cdd1188952d820b809df03
Hanif Samad <hanif.samad.sg@gmail.com>
2018-03-21 20:01:56 +0800

    Resolve #17. (#18)
    * Resolve #17.
    * Resolve #19.

--- generators/app/templates/src/route/healthcheck.ts
+++ generators/app/templates/src/route/healthcheck.ts
@@ -7,7 +7,7 @@ export function healthcheck(): express.Router {
   return express.Router({ mergeParams: true })
     .get('/', async (_, res, next) => {
       try {
-        return res.send(200).send('OK');
+        return res.status(200).send('OK');
       } catch (e) {
         return next(e);
       }
